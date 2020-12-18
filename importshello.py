96c17936d775bb4288a86697402c575301b72cc5
clemens-db <49700219+clemens-db@users.noreply.github.com>
2020-12-18 08:15:06 -0800

    Create importshello.py

---
+++ importshello.py
@@ -0,0 +1,12 @@
+# Add the current project directory to the system path.
+# We are working on making this just work without having to modify the sys.path
+import sys
+sys.path.append("/Workspace/Projects/clemens@databricks.com/rel-import")
+
+# Run code defined in neighboring python files
+import hello
+print(hello.world())
+
+# hello is a module imported from /Workspace/Projects/sai.suram@databricks.com/awesome/hello.py
+# Lets print and see python confirm that for us 
+print(hello)
